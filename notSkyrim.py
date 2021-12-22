6b039f25c21d3afa2cb7ab875b1fc13601f3e8b1
BlountTyler23 <816432@mystma.org>
2021-12-22 19:44:40 +0000

    1

--- notSkyrim.py
+++ notSkyrim.py
@@ -1,6 +1,8 @@
 import random
 points = 250
 
+print("You have 250 points to allocate to five different skills")
+
 h = int(input("How is your huberis?\n0-100\n>"))
 if h > 100 or h < 0 or h > points:
     print("Can you not count get out of here")
@@ -61,17 +63,48 @@ if start.lower() == "1":
     else:
         print("Well plowing field your plow hit a metal object in the ground lunching you over your plow landing over the cliff near by.")
         exit()
-    second = input("After finding the lengdary sword of Sting, what would you like to join King John Denver's war against North Virginia or rally the peasentry to overthrow the corrupt king? (To War/Rally the people)\n>")
-    if second.lower() == "to war":
-        print("You come to the king's aid. Out of greed, the king has you killed and steals the lengdary sword.")
-    if second.lower() == "rally the people":
-        print("You sneak into the capitial and stand before a group of people. You yell out by the gods our king is to corrupt lead the people of the lower class and the people in general. Rally behind me and behind this sword. as you stick Sting skywarrd.")
-
+    
 if start.lower() == "3": 
     roll = random.randrange(0, s) 
     if roll > 10:
-        print("well slaving away in the mines. you strike a hard rock boucing the pic back into your face. The foreman declares you legally dumb and sends you home to farm corn")
+        print("well slaving away in the mines. you strike a hard rock boucing the picaxe back into your face. The foreman declares you legally dumb and sends you home to farm corn")
 
     else:
-        print("well slaving away in the mines. you strike a hard rock boucing the pic back into your face rendring your self unconscious falling straight off a cliff.")
+        print("well slaving away in the mines. you strike a hard rock boucing the picaxe back into your face rendring your self unconscious falling straight off a cliff.")
         exit()
+roll = random.randrange(0, f)
+if roll > 20:
+        print("You plow your fields one day and find the burried sword of Sting")
+second = input("After finding the lengdary sword of Sting, what would you like to?\n 1. Join King John Denver's war against North Virginia\n 2. Rally the peasentry to overthrow the corrupt king?\n 3. Betray your people and join the Northerns\n>")
+if second.lower() == "1":
+        print("You come to the king's aid.")
+        roll = random.randrange(0, c) 
+        roll2 = random.randrange(0, h)
+        if roll > 40 and roll2 < 10:
+            print("You over come the kings evil greed he allows you to join the army as a captian")
+
+        else:
+            print("Out of greed, the king has you killed and steals the lengdary sword.")
+            exit()
+
+if second.lower() == "2":
+        print("You sneak into the capitial and stand before a group of people. You yell out by the gods our king is to corrupt lead the people of the lower class and the people in general. Rally behind me and behind this sword. as you stick Sting skywarrd.")
+        roll = random.randrange(0, c)
+        roll2 = random.randrange(0, h)
+        if roll > 75 and roll2 < 5:
+            print("The people rally behind you weapons in hand. Joined by many gaurds you over power the capital's loyalist.")
+        else:
+            print("Not swayed the people boo. Quickly the guards arrest you. ater three years in the dungeon you are beheaded")
+            exit()
+
+if second.lower() == "3":
+        print("You try to make you across the magical echanted boarder.")
+        roll = random.randrange(0, s)
+        roll2 = random.randrange(0, m)
+        roll2 = random.randrange(0, c)
+        if roll > 85 and roll2 > 90 and roll3 > 30:
+            print("You use disenchant spell dispelling the border wall and flee to the Northerns. The Northerns catch you but notice your carrying the lengdary sword, using quick words you meet with their king who allows you lead troops into the up coming battle.")
+        
+        else:
+            print("You run head long into the border hoping that your disenchant spell worked. Unfortunately you realize to late it failed. Runing into the shieled you're vaporized.")
+            exit()
